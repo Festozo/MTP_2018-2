71e538d510d2b84abf58c7269517521472c9ab48
Festozo <42520862+Festozo@users.noreply.github.com>
2018-10-13 13:52:51 -0300

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,25 @@
+//Vinícius Carvalho Festozo//
+//11811EEL018//
+
+
+
+#include <stdio.h>
+
+int main ()
+{
+
+	char seq[256];
+	int num[256], x = 0,j,i;
+	printf("\nDigite uma sequencia desejada : ");
+	scanf("%s", seq);	
+	for(i = 0; seq[i] != '\0'; i++) 
+		if(seq[i] >= 48 && seq[i] <= 57) 
+		{	
+			num[x] = seq[i] - 48; 
+			x++;	
+		}
+	
+	for(j = 0; j < x; j++) 
+	printf("%d", num[j]); 
+	return 0;
+}
