03a9129195bff45cdb85f6f18a7e8b441d6defe6
Festozo <42520862+Festozo@users.noreply.github.com>
2018-11-05 22:56:14 -0200

    Add files via upload

---
+++ P4.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int  func(int m, int n)
+{
+	if(m ==0)
+	{
+	return n+1;
+	}             
+	else if(m>0&&n==0)
+	{
+	func(m-1, 1);
+	}
+	else if(m>0&&n>0)
+	{
+	func(m-1,func(m,n-1));
+	}
+}
+int main ()
+{
+	int m,n;
+	printf("Coloque os valores para m e n nesta ordem.\n: ");
+	scanf("%i %i", &m, &n);
+	printf("O resultado final obtido eh : %i" ,func(m, n));
+	
+	return 0;
+}
