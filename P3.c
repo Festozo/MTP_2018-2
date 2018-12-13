363af97ff31926722d49f3a68a967990f7200e81
Festozo <42520862+Festozo@users.noreply.github.com>
2018-12-12 23:07:59 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -1,26 +1,29 @@
-/* P3.c */
-/*Vinícius Carvalho Festozo/*
-/*11811EEL018/*
-
+/*P3.c*/
+/*Vinicius Carvalho Festozo*/
+/*11811EEL018*/
 
 
 #include <stdio.h>
 
-int main ()
-{
 
-	char seq[256];
-	int num[256], x = 0,j,i;
-	printf("\nDigite uma sequencia desejada : ");
-	scanf("%s", seq);	
-	for(i = 0; seq[i] != '\0'; i++) 
-		if(seq[i] >= 48 && seq[i] <= 57) 
-		{	
-			num[x] = seq[i] - 48; 
-			x++;	
-		}
-	
-	for(j = 0; j < x; j++) 
-	printf("%d", num[j]); 
-	return 0;
+int main()
+{
+    char s1[256], s2[256];
+    int i, k=0, cum=0;
+    printf("Coloque sua sequencia de caracteres: ");
+    scanf("%s",s1);
+    for(i=0;s1[i]!='\0';i++)
+    {
+        if(s1[i]>='0' && s1[i]<='9')
+        {
+            s2[k]=s1[i];
+            k++;}
+    }
+    s2[k]='\0';
+    for(i=0;s2[i]!='\0';i++)
+    {
+			cum = cum*10 + s2[i] - '0';
+    }
+    printf("\n\n%d",cum);
+    return 0;
 }
